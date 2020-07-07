c95e62d9fa68e237c2d0e258d40e2bde306b9eeb
jayanthanala <javaoneapl@gmail.com>
2020-07-07 11:35:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,78 @@
+// require('dotenv').config()
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const fs = require('fs');
+var path = require('path');
+var multer  = require('multer');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+var upload = multer({ storage: storage })
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+
+
+mongoose.connect("mongodb+srv://manishreddy:laflame4033@webdatabase.rbrhg.mongodb.net/manishDB?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const imgSchema = new mongoose.Schema({
+    img: {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+const Image = mongoose.model("Image",imgSchema);
+
+app.use(jsonParser);
+
+app.get("/",(req,res) => {
+    Image.find({},function(err,results){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("pictures",{items: results,title: "My Recent Works"});
+        }
+    });
+});
+
+app.get("/upload",(req,res) => {
+    res.render("login");
+});
+
+app.post("/upload",(req,res) => {
+    if(req.body.username === "Manish" && req.body.password === "laflame"){
+        res.render("app",{Name: req.body.username});
+    }else{
+        res.send("Wrong Credential Entered");
+        // console.log(process.env.NAME, process.env.PASSWORD);
+    }
+});
+
+app.post('/imgupload', upload.single('image'), function (req, res, next) {
+    var image = new Image({
+        img: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            contentType: 'image/png'
+        }
+    });
+    image.save();
+    res.render("app",{Name: req.body.username});
+});
+
+app.listen( process.env.PORT || 3000,function(){
+    console.log("Server is up at "+process.env.PORT);
+});
